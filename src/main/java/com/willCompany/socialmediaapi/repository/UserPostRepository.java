ff9ef8ccc1d00cd565855ef77636bccf50625573
Tab0o33 <w.moinet@orange.fr>
2024-04-22 17:51:05 +0200

    refactor: suppression du nom de la BDD dans les requetes SQL

--- src/main/java/com/willCompany/socialmediaapi/repository/UserPostRepository.java
+++ src/main/java/com/willCompany/socialmediaapi/repository/UserPostRepository.java
@@ -14,33 +14,31 @@ import com.willCompany.socialmediaapi.utils.UserPostId;
 import jakarta.transaction.Transactional;
 
 @Repository
-public interface UserPostRepository extends CrudRepository<UserPost, UserPostId>{
-	
-	final String upsertQuery = "INSERT INTO db_socialmedia.user_post (user_id, post_id, is_liked) "
-	+ "VALUES (:userId, :postId, :isLiked) " 
-	+ "ON DUPLICATE KEY UPDATE is_liked = VALUES(is_liked)";
-	
-	final String getAllPostsWithDetailsQuery = 
-			  "SELECT p.id AS id, p.created_date as createdDate, p.image, p.text, "
-			+ "CONCAT(u.first_name, ' ', u.last_name) AS userName, u.profil_image as profilImage, "
-			+ "SUM(CASE WHEN up.is_liked = true THEN 1 ELSE 0 END) AS likeCount, "
-			+ "CASE WHEN EXISTS ( "
-			+ "		SELECT 1 "
-			+ "     FROM db_socialmedia.user_post "
-			+ "     WHERE post_id = p.id AND user_id = :userId AND is_liked = true "
-			+ ") THEN true ELSE false END AS isLiked "
-			+ "FROM db_socialmedia.posts p "
-			+ "LEFT JOIN db_socialmedia.user_post up ON p.id = up.post_id "
-			+ "JOIN db_socialmedia.users u ON p.user_id = u.id "
-			+ "GROUP BY p.id;";
-	
-	@Transactional
-	@Modifying
-	@Query(value = upsertQuery, nativeQuery = true)
-	void upsert(@Param("userId") Long userId, @Param("postId") Long postId, @Param("isLiked") boolean isLiked);
-	
-	@Query(value = getAllPostsWithDetailsQuery, nativeQuery = true)
-	List<Object[]> getAllPostsWithDetails(@Param("userId") Long userId);
-	
+public interface UserPostRepository extends CrudRepository<UserPost, UserPostId> {
+
+    final String upsertQuery = "INSERT INTO user_post (user_id, post_id, is_liked) "
+            + "VALUES (:userId, :postId, :isLiked) "
+            + "ON DUPLICATE KEY UPDATE is_liked = VALUES(is_liked)";
+
+    final String getAllPostsWithDetailsQuery = "SELECT p.id AS id, p.created_date as createdDate, p.image, p.text, "
+            + "CONCAT(u.first_name, ' ', u.last_name) AS userName, u.profil_image as profilImage, "
+            + "SUM(CASE WHEN up.is_liked = true THEN 1 ELSE 0 END) AS likeCount, "
+            + "CASE WHEN EXISTS ( "
+            + "		SELECT 1 "
+            + "     FROM user_post "
+            + "     WHERE post_id = p.id AND user_id = :userId AND is_liked = true "
+            + ") THEN true ELSE false END AS isLiked "
+            + "FROM posts p "
+            + "LEFT JOIN user_post up ON p.id = up.post_id "
+            + "JOIN users u ON p.user_id = u.id "
+            + "GROUP BY p.id;";
+
+    @Transactional
+    @Modifying
+    @Query(value = upsertQuery, nativeQuery = true)
+    void upsert(@Param("userId") Long userId, @Param("postId") Long postId, @Param("isLiked") boolean isLiked);
+
+    @Query(value = getAllPostsWithDetailsQuery, nativeQuery = true)
+    List<Object[]> getAllPostsWithDetails(@Param("userId") Long userId);
 
 }
